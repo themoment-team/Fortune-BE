02d8717c997492437e86e8e54b5e9745f4de6adb
hajeu <s22072@gsm.hs.kr>
2022-11-24 17:48:28 +0900

    setting

---
+++ src/main/java/fortune/be/fortunebackend/FortuneBackEndApplication.java
@@ -0,0 +1,13 @@
+package fortune.be.fortunebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortuneBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fortune/be/fortunebackend/FortuneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package fortune.be.fortunebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
